7447ade070b227c26d30bdcede73e74bc09f45c4
aLink56 <steven@waters-creative.co.uk>
2025-10-27 22:41:20 +0000

    init-0.2

---
+++ assets/main.js
@@ -0,0 +1,1 @@
+console.log("Dawnwind theme JS loaded");
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './**/*.liquid',
+    './**/*.js',
+    './**/*.json'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
